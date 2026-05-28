34529e9fb3abc357bb60a23941d3f5a1eb5c5f79
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-28 11:03:36 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class databaseSarseWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = databaseSarseWorker()
+    print("Worker engine initialized.")
